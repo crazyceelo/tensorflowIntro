f9ec42974d5bac71a57ff134c9ccfce5c382b07a
Carlos Perez <solracias@gmail.com>
2019-01-04 18:50:25 -0800

    now i know what different types of tensors are.

--- tutorial1.js
+++ tutorial1.js
@@ -1,4 +1,25 @@
 const tf = require("@tensorflow/tfjs");
 require("@tensorflow/tfjs-node");
 
-tf.tensor([1, 2, 3, 4]).print();
+const firstTensor = tf.tensor([1, 2, 3, 4, 5]);
+console.log(firstTensor.toString());
+
+const tensoreWithShape = tf.tensor([1, 2, 3, 4], [2, 2]).print();
+
+const twoByTwoByTwo = tf
+  .tensor([1, 2, 3, 4, 5, 6, 7, 8], [2, 2, 2], "int32")
+  .print();
+
+const values = [];
+for (let i = 0; i < 30; i++) {
+  values[i] = Math.floor(Math.random() * 101);
+}
+
+const shape = [2, 5, 3];
+const data = tf.tensor(values, shape);
+
+// console.log(data.toString());
+// console.log(data);
+
+const threeD = tf.tensor3d(values, shape, "int32");
+console.log(threeD.toString());
