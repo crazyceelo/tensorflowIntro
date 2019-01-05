1b1e4693a52f75b57f4700090ba20f3ba91518c2
Carlos Perez <solracias@gmail.com>
2019-01-04 19:50:08 -0800

    tf.tidy and dispose methods.

--- variables.js
+++ variables.js
@@ -2,13 +2,15 @@ const tf = require("@tensorflow/tfjs");
 require("@tensorflow/tfjs-node");
 
 const values = [];
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 15; i++) {
   values[i] = Math.floor(Math.random() * 101);
 }
 
-const shape = [2, 5, 3];
+const shape = [5, 3];
 
-const tense = tf.tensor3d(values, shape, "int32");
+const a = tf.tensor3d(values, shape, "int32");
+const b = tf.tensor3d(values, shape, "int32");
+const bb = b.transpose(); // turns shape into [3,5]
 
-const vtense = tf.variable(tense);
-console.log(vtense);
+const sum = a.matMul(bb);
+sum.print();
